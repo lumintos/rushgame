fe47f45310b6d79c0d7d9a6901c65102a0a2208a
truongngoctuan <tntuan0712494@gmail.com>
2013-12-18 13:07:44 +0100

    local commit

--- Assets/Scripts/PlayerScripts/PlayerHashIDs.cs
+++ Assets/Scripts/PlayerScripts/PlayerHashIDs.cs
@@ -5,8 +5,11 @@ public class PlayerHashIDs : MonoBehaviour
 {
 	public static int idleState 			= Animator.StringToHash("Base Layer.Idle");
 	public static int locomotionState		= Animator.StringToHash("Base Layer.Locomotion");
-	public static int jumpState				= Animator.StringToHash("Base Layer.Jump");
+	public static int jumpState				= Animator.StringToHash("JumpProcess.Jump");
+	public static int fallState				= Animator.StringToHash("JumpProcess.Fall");
+	public static int landState				= Animator.StringToHash("JumpProcess.Land");
 
 	public static int speedFloat			= Animator.StringToHash("speed");
 	public static int JumpBool				= Animator.StringToHash("Jump");
+	public static int FallToLandBool		= Animator.StringToHash("FallToLand");
 }
