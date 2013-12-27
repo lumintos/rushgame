793f15d90f0d14c2fde1b5deac76333f79347004
hieurl <luongtronghieu@gmail.com>
2013-12-27 23:18:05 +0100

    Update length value for status to 1 and user password to 128 to fit with sha512 hash function used

--- Server/rush.sql
+++ Server/rush.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 27, 2013 at 08:19 PM
+-- Generation Time: Dec 27, 2013 at 11:16 PM
 -- Server version: 5.5.34
 -- PHP Version: 5.3.10-1ubuntu3.9
 
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `RushFriendship` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned DEFAULT NULL,
   `friend_id` int(10) unsigned DEFAULT NULL,
-  `status` int(11) NOT NULL DEFAULT '1',
+  `status` int(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `RushItem` (
   `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `desc` text COLLATE utf8_unicode_ci NOT NULL,
   `price` double unsigned NOT NULL DEFAULT '0',
-  `status` int(11) NOT NULL DEFAULT '1',
+  `status` int(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `RushScore` (
   `lose` int(10) unsigned NOT NULL,
   `spirit` int(10) unsigned NOT NULL,
   `user_id` int(10) unsigned DEFAULT NULL,
-  `status` int(11) NOT NULL DEFAULT '1',
+  `status` int(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
@@ -75,7 +75,7 @@ CREATE TABLE IF NOT EXISTS `RushSkill` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `skill_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `desc` text COLLATE utf8_unicode_ci NOT NULL,
-  `status` int(11) NOT NULL DEFAULT '1',
+  `status` int(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
@@ -88,12 +88,20 @@ CREATE TABLE IF NOT EXISTS `RushSkill` (
 CREATE TABLE IF NOT EXISTS `RushUser` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `password` text COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
   `score_table_id` int(11) DEFAULT NULL,
   `skill_id` int(10) unsigned DEFAULT NULL,
-  `status` int(11) NOT NULL DEFAULT '1',
+  `is_online` int(1) unsigned NOT NULL DEFAULT '0',
+  `status` int(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `RushUser`
+--
+
+INSERT INTO `RushUser` (`id`, `username`, `password`, `score_table_id`, `skill_id`, `is_online`, `status`) VALUES
+(1, 'test', 'ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff', NULL, NULL, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -105,7 +113,7 @@ CREATE TABLE IF NOT EXISTS `RushUserItem` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned DEFAULT NULL,
   `item_id` int(10) unsigned DEFAULT NULL,
-  `status` int(11) NOT NULL DEFAULT '1',
+  `status` int(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
@@ -119,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `RushUserSkill` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned DEFAULT NULL,
   `skill_id` int(10) unsigned DEFAULT NULL,
-  `status` int(11) NOT NULL DEFAULT '1',
+  `status` int(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
