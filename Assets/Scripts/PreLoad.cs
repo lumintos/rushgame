11f184d6fca1c5a63ed8c3cfe92b53afa770354d
lumintos <giangdam.mobserv@gmail.com>
2013-12-30 12:08:11 +0100

    Fix bug logout

---
+++ Assets/Scripts/PreLoad.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Application.LoadLevel("login");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
