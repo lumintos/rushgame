86c49746aadeee30a82a95373b6566523e4bbc21
LAM Cao Vien <lamcaovien@gmail.com>
2014-01-26 01:12:11 +0100

    Update new map, and add in game button setting UI

---
+++ Assets/Scripts/CameraFacingBillboard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+	public Camera m_Camera;
+
+	void Awake()
+	{
+		m_Camera = Camera.main;
+	}
+
+	void Update()
+	{
+
+		transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.back,
+		                 m_Camera.transform.rotation * Vector3.up);
+	}
+}
